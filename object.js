06a0b965c2a12c3a726d41e6f1a576e99b3a3280
Thiago Lanza <39680004+lanzath@users.noreply.github.com>
2020-02-23 17:34:17 -0300

    Resolução de Construção e impressão de objetos

---
+++ object.js
@@ -0,0 +1,12 @@
+//declaração do objeto
+const enterprise = {
+    name: 'Rocketseat',
+    color: 'Roxo',
+    focus: 'Programação',
+    address: {
+        street: 'Rua Guilherme Gembala',
+        number: 260
+    }
+}
+console.log(`A Empresa ${enterprise.name} está localizada em ${enterprise.address.street},
+${enterprise.address.number}`);
